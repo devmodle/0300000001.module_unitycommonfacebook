bde001a64f33a56a3b05cc0ad9f00d63768612e7
dante <are2341@nate.com>
2020-09-09 03:11:39 +0900

    [이상동] 패키지 갱신

--- Scripts/Runtime/Manager/CFacebookManager+Analytics.cs
+++ Scripts/Runtime/Manager/CFacebookManager+Analytics.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 #if FACEBOOK_MODULE_ENABLE && FACEBOOK_ANALYTICS_ENABLE
 using Facebook.Unity;
 
+#if PURCHASE_MODULE_ENABLE
+using UnityEngine.Purchasing;
+#endif			// #if PURCHASE_MODULE_ENABLE
+
 //! 페이스 북 관리자 - 분석
 public partial class CFacebookManager : CSingleton<CFacebookManager> {
 		#region 함수
@@ -35,19 +39,38 @@ public partial class CFacebookManager : CSingleton<CFacebookManager> {
 #if MSG_PACK_ENABLE
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_DEVICE_ID, CCommonAppInfoStorage.Instance.AppInfo.DeviceID);
 
-#if AUTO_LOG_PARAM_ENABLE
+#if AUTO_LOG_PARAMS_ENABLE
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_PLATFORM, CCommonAppInfoStorage.Instance.PlatformName);
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_USER_TYPE, CCommonUserInfoStorage.Instance.UserInfo.UserType.ToString());
 			
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_LOG_TIME, System.DateTime.UtcNow.ExToLongString());
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_INSTALL_TIME, CCommonAppInfoStorage.Instance.AppInfo.UTCInstallTime.ExToLongString());
-#endif			// #if AUTO_LOG_PARAM_ENABLE
+#endif			// #if AUTO_LOG_PARAMS_ENABLE
 #endif			// #if MSG_PACK_ENABLE
 
 			FB.LogAppEvent(a_oName, a_oValue, oDataList);
 		}
 #endif			// #if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
 	}
-	#endregion			// 함수	
+	#endregion			// 함수
+
+	#region 조건부 함수
+#if PURCHASE_MODULE_ENABLE
+	//! 결제 로그를 전송한다
+	public void SendPurchaseLog(Product a_oProduct, Dictionary<string, object> a_oDataList) {
+		CAccess.Assert(a_oProduct != null);
+		CFunc.ShowLog("CFacebookManager.SendPurchaseLog: {0}", KCDefine.B_LOG_COLOR_PLUGIN, a_oProduct);
+
+#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
+#if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
+		// 초기화 되었을 경우
+		if(this.IsInit) {
+			FB.LogPurchase(a_oProduct.metadata.localizedPrice, a_oProduct.metadata.isoCurrencyCode, a_oDataList);
+		}
+#endif			// #if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
+#endif			// #if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
+	}
+#endif			// #if PURCHASE_MODULE_ENABLE
+	#endregion			// 조건부 함수
 }
 #endif			// #if FACEBOOK_MODULE_ENABLE && FACEBOOK_ANALYTICS_ENABLE
