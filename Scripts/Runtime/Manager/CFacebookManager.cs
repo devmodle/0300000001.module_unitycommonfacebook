bd0e6c0442072dfae989d14fd9c5b02baa48a2a7
sd.lee <sd.lee@ninetap.com>
2021-04-22 18:05:00 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CFacebookManager.cs
+++ Scripts/Runtime/Manager/CFacebookManager.cs
@@ -17,7 +17,7 @@ public partial class CFacebookManager : CSingleton<CFacebookManager> {
 	public bool IsInit {
 		get {
 #if UNITY_IOS || UNITY_ANDROID
-			return !CAccess.IsMobile() ? false : FB.IsInitialized;
+			return !CAccess.IsMobile ? false : FB.IsInitialized;
 #else
 			return false;
 #endif			// #if UNITY_IOS || UNITY_ANDROID
