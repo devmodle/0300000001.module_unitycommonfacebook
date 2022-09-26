a1c5c98c15cdb60682b97cc8108086edd8855bd4
dante <lkstudio.dante@gmail.com>
2022-09-26 21:42:20 +0900

    [이상동]

--- Scripts/Runtime/Manager/CFacebookManager+Auth.cs
+++ Scripts/Runtime/Manager/CFacebookManager+Auth.cs
@@ -9,7 +9,7 @@ using Facebook.Unity;
 
 /** 페이스 북 관리자 - 인증 */
 public partial class CFacebookManager : CSingleton<CFacebookManager> {
-	#region 함수
+#region 함수
 	/** 로그인을 처리한다 */
 	public void Login(List<string> a_oPermissionList, System.Action<CFacebookManager, bool> a_oCallback, System.Action<CFacebookManager, bool> a_oChangeViewStateCallback = null) {
 		CFunc.ShowLog($"CFacebookManager.Login: {a_oPermissionList}", KCDefine.B_LOG_COLOR_PLUGIN);
@@ -27,7 +27,7 @@ public partial class CFacebookManager : CSingleton<CFacebookManager> {
 		}
 #else
 		CFunc.Invoke(ref a_oCallback, this, false);
-#endif			// #if UNITY_IOS || UNITY_ANDROID
+#endif         // #if UNITY_IOS || UNITY_ANDROID                                           
 	}
 
 	/** 로그아웃을 처리한다 */
@@ -40,21 +40,21 @@ public partial class CFacebookManager : CSingleton<CFacebookManager> {
 			if(this.IsInit && this.IsLogin) {
 				FB.LogOut();
 			}
-#endif			// #if UNITY_IOS || UNITY_ANDROID
+#endif         // #if UNITY_IOS || UNITY_ANDROID                                           
 		} finally {
 			CScheduleManager.Inst.AddCallback(KCDefine.U_KEY_FACEBOOK_M_LOGOUT_CALLBACK, () => CFunc.Invoke(ref a_oCallback, this));
 		}
 	}
-	#endregion			// 함수
+#endregion         // 함수               
 
-	#region 조건부 함수
+#region 조건부 함수
 #if UNITY_IOS || UNITY_ANDROID
 	/** 로그인 되었을 경우 */
 	private void OnLogin(ILoginResult a_oResult) {
 		CFunc.ShowLog($"CFacebookManager.OnLogin: {this.IsLogin}, {a_oResult}", KCDefine.B_LOG_COLOR_PLUGIN);
 		CScheduleManager.Inst.AddCallback(KCDefine.U_KEY_FACEBOOK_M_LOGIN_CALLBACK, () => this.CallbackDict.GetValueOrDefault(EFacebookCallback.LOGIN)?.Invoke(this, this.IsLogin));
 	}
-#endif			// #if UNITY_IOS || UNITY_ANDROID
-	#endregion			// 조건부 함수
+#endif         // #if UNITY_IOS || UNITY_ANDROID                                           
+#endregion         // 조건부 함수                   
 }
-#endif			// #if FACEBOOK_MODULE_ENABLE
+#endif         // #if FACEBOOK_MODULE_ENABLE                                       
